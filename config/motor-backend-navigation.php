963c754653b2443ced195f91946e9eea7dcb3b97
Reza Esmaili <bitbucket@dfox.info>
2021-05-16 22:46:00 +0200

    Change navigation routes

--- config/motor-backend-navigation.php
+++ config/motor-backend-navigation.php
@@ -4,15 +4,15 @@ return [
     'items' => [
         120 => [
             'slug'        => 'partymeister-competitions',
-            'name'        => 'partymeister-competitions::backend/global.competitions',
-            'icon'        => 'fa fa-trophy',
+            'name'        => 'partymeister-competitions.global.competitions',
+            'icon'        => 'trophy',
             'route'       => null,
             'roles'       => ['SuperAdmin'],
             'permissions' => ['competitions.read'],
             'items'       => [
                 100 => [ // <-- !!! replace 170 with your own sort position !!!
                          'slug'        => 'competitions',
-                         'name'        => 'partymeister-competitions::backend/competitions.competitions',
+                         'name'        => 'partymeister-competitions.competitions.competitions',
                          'icon'        => 'fa fa-angle-right',
                          'route'       => 'backend.competitions.index',
                          'roles'       => ['SuperAdmin'],
@@ -20,7 +20,7 @@ return [
                 ],
                 110 => [ // <-- !!! replace 867 with your own sort position !!!
                          'slug'        => 'entries',
-                         'name'        => 'partymeister-competitions::backend/entries.entries',
+                         'name'        => 'partymeister-competitions.entries.entries',
                          'icon'        => 'fa fa-angle-right',
                          'route'       => 'backend.entries.index',
                          'roles'       => ['SuperAdmin'],
@@ -28,7 +28,7 @@ return [
                 ],
                 120 => [ // <-- !!! replace 893 with your own sort position !!!
                          'slug'        => 'option_groups',
-                         'name'        => 'partymeister-competitions::backend/option_groups.option_groups',
+                         'name'        => 'partymeister-competitions.option_groups.option_groups',
                          'icon'        => 'fa fa-angle-right',
                          'route'       => 'backend.option_groups.index',
                          'roles'       => ['SuperAdmin'],
@@ -36,7 +36,7 @@ return [
                 ],
                 130 => [ // <-- !!! replace 318 with your own sort position !!!
                          'slug'        => 'competition_types',
-                         'name'        => 'partymeister-competitions::backend/competition_types.competition_types',
+                         'name'        => 'partymeister-competitions.competition_types.competition_types',
                          'icon'        => 'fa fa-angle-right',
                          'route'       => 'backend.competition_types.index',
                          'roles'       => ['SuperAdmin'],
@@ -44,7 +44,7 @@ return [
                 ],
                 140 => [ // <-- !!! replace 929 with your own sort position !!!
                          'slug'        => 'vote_categories',
-                         'name'        => 'partymeister-competitions::backend/vote_categories.vote_categories',
+                         'name'        => 'partymeister-competitions.vote_categories.vote_categories',
                          'icon'        => 'fa fa-angle-right',
                          'route'       => 'backend.vote_categories.index',
                          'roles'       => ['SuperAdmin'],
@@ -52,7 +52,7 @@ return [
                 ],
                 150 => [ // <-- !!! replace 617 with your own sort position !!!
                          'slug'        => 'access_keys',
-                         'name'        => 'partymeister-competitions::backend/access_keys.access_keys',
+                         'name'        => 'partymeister-competitions.access_keys.access_keys',
                          'icon'        => 'fa fa-plus',
                          'route'       => 'backend.access_keys.index',
                          'roles'       => ['SuperAdmin'],
@@ -60,7 +60,7 @@ return [
                 ],
                 170 => [ // <-- !!! replace 992 with your own sort position !!!
                          'slug'        => 'votes',
-                         'name'        => 'partymeister-competitions::backend/votes.votes',
+                         'name'        => 'partymeister-competitions.votes.votes',
                          'icon'        => 'fa fa-plus',
                          'route'       => 'backend.votes.index',
                          'roles'       => ['SuperAdmin'],
