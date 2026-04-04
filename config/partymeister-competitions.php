26ff9975bc5dac55ed7d3d02bdbac719b92863ac
Reza Esmaili <github@dfox.info>
2026-04-04 19:26:16 +0200

    Rename export token env var to PARTYMEISTER_EXPORT_TOKEN

--- config/partymeister-competitions.php
+++ config/partymeister-competitions.php
@@ -5,5 +5,5 @@ return [
     // in a competition have their final file confirmed (final_file_media_id set).
     'require_all_final_files_for_release' => (bool) env('PM_REQUIRE_ALL_FINAL_FILES_FOR_RELEASE', false),
     'shader_showdown_token' => env('SHADER_SHOWDOWN_TOKEN', ''),
-    'export_token' => env('PM_EXPORT_TOKEN', ''),
+    'export_token' => env('PARTYMEISTER_EXPORT_TOKEN', ''),
 ];
